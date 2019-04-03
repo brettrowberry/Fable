23286d7534239742e442cea56069065aaf0a9387
Brett Rowberry <brett@Bretts-MacBook-Pro.local>
2019-04-02 22:58:19 -0500

    cleared some comments

--- Fable/src/Client/Client.fs
+++ Fable/src/Client/Client.fs
@@ -17,8 +17,6 @@ open Fulma
 
 type Model = { Accounts : FableStorageAccount [] }
 
-// The Msg type defines what events/actions can occur while the application is running
-// the state of the application changes *only* in reaction to these events
 type Msg =
 | ListAccounts of Result<FableStorageAccount [], exn>
 | Create of string
@@ -27,7 +25,6 @@ type Msg =
 let fetchAccounts () =
   fetchAs ("http://localhost:8080" + Route.builder Route.List) (Decode.Auto.generateDecoder<FableStorageAccount []>()) []
 
-// defines the initial state and initial command (= side-effect) of the application
 let init () : Model * Cmd<Msg> =
     let initialModel = { Accounts = [||] }
     let loadCountCmd =
@@ -38,9 +35,6 @@ let init () : Model * Cmd<Msg> =
             (Error >> ListAccounts)
     initialModel, loadCountCmd
 
-// The update function computes the next state of the application based on the current state and the incoming events/messages
-// It can also run side-effects (encoded as commands) like calling the server via Http.
-// these commands in turn, can dispatch messages to which the update function will react.
 let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
     match msg with
     | ListAccounts (Ok accounts)->
